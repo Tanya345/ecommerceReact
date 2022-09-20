05dd79f5423f94602f35bdd7bbe97db6e8382621
tanya.bansal-at-326184364317 <tanya.bansal@flexiele.com>
2022-09-20 14:59:38 +0530

    added _redirects

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React from 'react'
 // import { AppContext } from '../App'
 import {useNavigate} from 'react-router-dom'
 
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-import { resolve } from "path"
-import vm from 'vm-browserify'
-
-module.exports = {
-	resolve:{
-		fallback:{ vm: require.resolve("vm-browserify") }
-	}
-}
